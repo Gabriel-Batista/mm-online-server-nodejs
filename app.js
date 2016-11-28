b747890bfb623c598b1edd2dbdf788477f4450d2
LilEggz <gabedbatista@gmail.com>
2016-11-28 03:11:51 -0500

    added listeners.

--- app.js
+++ app.js
@@ -23,11 +23,58 @@ io.on('connection', function (socket) {
 
   // handler for disconnect
   // this will run when a user closes the stream.
-  socket.on('disconnect', function () {
+  socket.on('user disconnect', function () {
     // tell everyone who disconnected.
     io.emit('user disconnected', {});
     console.log('user disconnected');
   });
+
+});
+
+// handler for movement
+// this will run when a user moves
+socket.on('user movement', function () {
+  // tell everyone who moved.
+  io.emit('user moved', {});
+  console.log('user moved');
+});
+
+});
+// handler for shooting weapon.
+// this will run when a user shoots his gun.
+socket.on('user fired', function () {
+  // tell everyone who shot and in which direction.
+  io.emit('user fired', {});
+  console.log('user fired');
+});
+
+});
+// handler for jumping.
+// this will run when a user jumps.
+socket.on('user jumped', function () {
+  // tell everyone who jumped.
+  io.emit('user jumped', {});
+  console.log('user jumped');
+});
+
+});
+// handler for dying.
+// this will run when a user dies.
+socket.on('user died', function () {
+  // tell everyone who died.
+  io.emit('user died', {});
+  console.log('user died');
+});
+
+});
+
+// handler for respawn
+// this will run when a user respawns.
+socket.on('user respawned', function () {
+  // tell everyone who respawned.
+  io.emit('user respawned', {});
+  console.log('user respawned');
+});
 
 });
 
